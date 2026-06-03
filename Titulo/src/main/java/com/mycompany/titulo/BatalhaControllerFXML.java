fea96fc21dbc5ff60bd3fb93ac3e75bdeee0f7aa
tembiapo <paulo.jesus5@estudante.ifms.edu.br>
2026-06-03 14:18:33 -0400

    feat(melhorias no fxml): adicionado uma capa para o inicio do jogo, deixado o batalha mais visual

---
+++ Titulo/src/main/java/com/mycompany/titulo/BatalhaControllerFXML.java
@@ -0,0 +1,111 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.mycompany.titulo;
+
+import Controller.BatalhaController;
+import Model.Batalha;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+
+/**
+ * FXML Controller class
+ *
+ * @author user
+ */
+public class BatalhaControllerFXML implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    @FXML
+    private Label labelTurno;
+    @FXML
+    private Label labelNomeHeroi;
+    @FXML
+    private Label labelVidaHeroi;
+    @FXML
+    private ProgressBar barVidaHeroi;
+    @FXML
+    private Label labelNomeInimigo;
+    @FXML
+    private Label labelVidaInimigo;
+    @FXML
+    private ProgressBar barVidaInimigo;
+    @FXML
+    private Button btnAtacar;
+    @FXML
+    private Button btnDefender;
+    @FXML
+    private Button btnMagia;
+    @FXML
+    private Button btnUsarItem;
+
+    private Batalha batalha;
+    private BatalhaController batalhaController = new BatalhaController();
+    
+    
+
+    public void setBatalha(Batalha batalha) {
+        this.batalha = batalha;
+        atualizarTela();
+    }
+
+    private void atualizarTela() {
+        labelTurno.setText("Turno " + batalha.getTurnoAtual());
+
+        labelNomeHeroi.setText(batalha.getHeroi().getNome());
+        labelVidaHeroi.setText(batalha.getHeroi().getVida() + " / " + batalha.getHeroi().getVidaMx());
+        barVidaHeroi.setProgress((double) batalha.getHeroi().getVida() / batalha.getHeroi().getVidaMx());
+
+        labelNomeInimigo.setText(batalha.getInimigo().getNome());
+        labelVidaInimigo.setText(batalha.getInimigo().getVida() + " / " + batalha.getInimigo().getVidaMx());
+        barVidaInimigo.setProgress((double) batalha.getInimigo().getVida() / batalha.getInimigo().getVidaMx());
+
+        // desabilita botões se batalha encerrada
+        boolean encerrada = batalha.isBatalhaEncerrada();
+        btnAtacar.setDisable(encerrada);
+        btnDefender.setDisable(encerrada);
+        btnMagia.setDisable(encerrada);
+        btnUsarItem.setDisable(encerrada);
+    }
+
+    @FXML
+    private void handleAtacar() {
+        batalhaController.Atacar(batalha);
+        batalhaController.Atacar(batalha);
+        atualizarTela();
+    }
+
+    @FXML
+    private void handleDefender() {
+        batalhaController.Defender(batalha);
+        batalhaController.Atacar(batalha);
+        atualizarTela();
+    }
+
+//    @FXML
+//    private void handleMagia() {
+//        batalhaController.usarMagia(batalha);
+//        batalhaController.inimigoAtacar(batalha);
+//        atualizarTela();
+//    }
+
+    @FXML
+    private void handleUsarItem() {
+        // abre tela de seleção de item
+    }    
+    
+    
+}
--- Titulo/src/main/java/com/mycompany/titulo/InicioController.java
+++ Titulo/src/main/java/com/mycompany/titulo/InicioController.java
@@ -1,9 +1,25 @@
 package com.mycompany.titulo;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.web.WebView;
 
-public class InicioController {
+public class InicioController implements Initializable {
+    @FXML
+    private WebView gifView;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        String gifPath = getClass().getResource("/com/mycompany/titulo/imagens/fundoPegandoFogo.gif").toExternalForm();
+        gifView.getEngine().loadContent(
+                "<html><body style='margin:0;background:black;'>"
+                + "<img src='" + gifPath + "' width='100%' height='100%'/>"
+                + "</body></html>"
+        );
+    }
 
     @FXML
     private void actionIniciar() throws IOException {
