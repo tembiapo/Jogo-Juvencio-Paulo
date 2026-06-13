40275773989ed5a50080c268fcae2617d6db5870
tembiapo <paulo.jesus5@estudante.ifms.edu.br>
2026-06-13 08:37:54 -0400

    feat(batalha): agora tem um sistema de batalha

--- Titulo/src/main/java/Controller/BatalhaController.java
+++ Titulo/src/main/java/Controller/BatalhaController.java
@@ -2,7 +2,9 @@ package Controller;
 
 import Dao.DaoInimigo;
 import Model.Batalha;
+import Model.Capanga;
 import Model.Escudo;
+import Model.Heroi;
 import Model.Personagem;
 
 /**
@@ -21,19 +23,19 @@ public class BatalhaController {
         inimigo.setVida(inimigo.getVida() - danoFinal);
         System.out.println(jogador.getNome() + " atacou causando " + danoFinal + " de dano!");
 
-        // inimigo contra-ataca — jogador NÃO rola dado, só usa atributo fixo (armaduras)
-        int danoInimigoAtaque = inimigo.calcularDanoAtaque();
-        int defesaFixaJogador = (jogador.getDefesa() + jogador.getArmaduraBotas().getDefesaBase() + jogador.getArmaduraCalca().getDefesaBase()
-                                + jogador.getArmaduraPeitoral().getDefesaBase() + jogador.getArmaduraCapacete().getDefesaBase()) 
-                                - inimigo.getAgilidade(); // sem dado!
+//        // inimigo contra-ataca — jogador NÃO rola dado, só usa atributo fixo (armaduras)
+//        int danoInimigoAtaque = inimigo.calcularDanoAtaque();
+//        int defesaFixaJogador = (jogador.getDefesa() + jogador.getArmaduraBotas().getDefesaBase() + jogador.getArmaduraCalca().getDefesaBase()
+//                                + jogador.getArmaduraPeitoral().getDefesaBase() + jogador.getArmaduraCapacete().getDefesaBase()) 
+//                                - inimigo.getAgilidade(); // sem dado!
         //se for escudo
-        if (jogador.getMaoEsquerda() instanceof  Escudo) {
-            Escudo armaEsquerda = (Escudo) jogador.getMaoEsquerda();
-            defesaFixaJogador += armaEsquerda.getBloqueioBase();
-        }
-        int danoFinalInimigo = Math.max(0, danoInimigoAtaque - defesaFixaJogador);
-        jogador.setVida(jogador.getVida() - danoFinalInimigo);
-        System.out.println(inimigo.getNome() + " contra-atacou causando " + danoFinalInimigo + " de dano!");
+//        if (jogador.getMaoEsquerda() instanceof  Escudo) {
+//            Escudo armaEsquerda = (Escudo) jogador.getMaoEsquerda();
+//            defesaFixaJogador += armaEsquerda.getBloqueioBase();
+//        }
+//        int danoFinalInimigo = Math.max(0, danoInimigoAtaque - defesaFixaJogador);
+//        jogador.setVida(jogador.getVida() - danoFinalInimigo);
+//        System.out.println(inimigo.getNome() + " contra-atacou causando " + danoFinalInimigo + " de dano!");
 
         batalha.proximoTurno();
     }
@@ -54,9 +56,57 @@ public class BatalhaController {
         batalha.proximoTurno();
     }
     
-    
-    public void IniciarBatalha(){
-        DaoInimigo daoInimigo = new DaoInimigo();
-        
+    public void InimigoAtacar(Batalha batalha) {
+        Personagem jogador = batalha.getHeroi();
+        Personagem inimigo = batalha.getInimigo();
+
+        int danoInimigoAtaque = inimigo.calcularDanoAtaque();
+        int defesaFixaJogador = (jogador.getDefesa() + jogador.getArmaduraBotas().getDefesaBase() + jogador.getArmaduraCalca().getDefesaBase()
+                                + jogador.getArmaduraPeitoral().getDefesaBase() + jogador.getArmaduraCapacete().getDefesaBase()) 
+                                - inimigo.getAgilidade(); // sem dado!
+
+        //se for escudo
+        if (jogador.getMaoEsquerda() instanceof  Escudo) {
+            Escudo armaEsquerda = (Escudo) jogador.getMaoEsquerda();
+            defesaFixaJogador += armaEsquerda.getBloqueioBase();
+        }
+        int danoFinalInimigo = Math.max(0, danoInimigoAtaque - defesaFixaJogador);
+        jogador.setVida(jogador.getVida() - danoFinalInimigo);
+        System.out.println(inimigo.getNome() + " contra-atacou causando " + danoFinalInimigo + " de dano!");
     }
+    
+    public void IniciarBatalha(){        
+        Heroi heroi = Sessao.getHeroiAtual();
+        
+        if (heroi == null) {
+            System.out.println("\nSessao sem heroi\n");
+            return;
+        }
+        
+        try {
+            DaoInimigo daoInimigo = new DaoInimigo();
+            Capanga inimigo = null;
+            int nivel  = 0;
+
+            if (heroi.getDiasPassados() > 10) {                
+                nivel = heroi.getNivel()+ 2;
+                inimigo = daoInimigo.getCapanga(nivel);                
+            }else{
+                nivel = heroi.getNivel();
+                inimigo = daoInimigo.getCapanga(nivel);
+            }
+            
+            if (inimigo == null) {
+                System.err.println("Nenhum inimigo encontrado para o nível: " + nivel);
+                return;
+            }
+            
+            Batalha batalha = new Batalha(heroi, inimigo);
+            Sessao.setBatalhaAtual(batalha);
+        } catch (Exception e) {
+            System.err.println("Erro ao iniciar batalha: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+    
 }
--- Titulo/src/main/java/Controller/Sessao.java
+++ Titulo/src/main/java/Controller/Sessao.java
@@ -4,6 +4,7 @@
  */
 package Controller;
 
+import Model.Batalha;
 import Model.Heroi;
 
 /**
@@ -12,9 +13,18 @@ import Model.Heroi;
  */
 public class Sessao {
     private static Heroi heroiAtual;
+    private static Batalha batalhaAtual;
 
     public static Heroi getHeroiAtual() {
         return heroiAtual;
+    }
+    
+    public static Batalha getBatalhaAtual(){
+        return batalhaAtual;
+    }
+    
+    public static void setBatalhaAtual(Batalha batalha){
+        batalhaAtual = batalha;
     }
 
     public static void setHeroiAtual(Heroi heroi) {
@@ -23,5 +33,9 @@ public class Sessao {
 
     public static void encerrar() {
         heroiAtual = null;
+    }
+    
+    public static void encerrarBatalhaAtual(){
+        batalhaAtual = null;
     }
 }
